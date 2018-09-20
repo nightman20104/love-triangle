16fb95e3b6ed7e681b9d19f009e052cc2897e95b
nightman20104 <programist20104@gmail.com>
2018-09-19 22:35:39 -0700

    Exercise 2

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+let count = 0;
+let spichonee_1,spichonee_2;
+for (let i=0; i<preferences.length; i++) {
+  spichonee_1 = preferences[i] -1;
+  spichonee_2 = preferences[spichonee_1] -1;
+  if (preferences[spichonee_2] -1 == i) {
+    count+=1;
+  }
+}
+return result = Math.floor(count / 3);
 };
